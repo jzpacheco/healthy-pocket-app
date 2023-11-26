6da6c7ebf08c41db6f39d65f578891d7eb33e8e5
jzpacheco <jeffersonpacheco@hotmail.com>
2023-11-25 22:48:00 -0300

    feat: barebone of home, transaction and analytics screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HomeScreen(props) {
+    return(
+        <View>
+            <Text>Saldo: R$9.999,00</Text>
+            <Text>Despesa: R$123</Text>
+            <Text>Receita: R$10.000</Text>
+        </View>
+    )
+
+}
---
+++ src/screens/TransactionScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function TransactionScreen(props){
+    return(
+        <View>
+            <Text>Nome:</Text>
+            <Text>Tipo:</Text>
+            <Text>Valor:</Text>
+            <Text>Recorrente:</Text>
+        </View>
+    )
+}
